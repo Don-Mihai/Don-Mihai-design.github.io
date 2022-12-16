d7c59b6b517e7c2ca41af6ed8a820987013bf8c9
Palienko Mihai <palienko@advantum.pro>
2022-12-16 21:40:27 +0300

    [feat] disable button form

---
+++ ifElse.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// let year = Number(prompt('Сколько вам лет?', ''));
+
+
+// if(year < 18) {
+//     console.log('Вы попали не на тот сайт')
+// }
+
+// if(year > 18) {
+//     console.log('Наслаждайтесь просмотром!')
+// }
+
+// if(year === 18) {
+//     console.log('Вам точно 18 лет?')
+// }
+
+
+
+// let text;
+
+// if(year > 18) {
+//     text = 'Вам больше 18'
+// } else {
+//     text = 'Вам меньше 18'
+// }
+
+
+// let text = year > 18 ? 'Вам больше 18' : 'Вам меньше 18';
+
+// console.log(text)
