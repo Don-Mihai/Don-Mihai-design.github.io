cf28ed185e668b80dad7427a6401f7058e2d16e8
Palienko Mihai <palienko@advantum.pro>
2023-01-13 22:03:13 +0300

    func obj

---
+++ functions.js
@@ -0,0 +1,74 @@
+//  Function Declaration
+
+// sayHi()
+
+// function sayHi() {
+//     alert( "Привет" );
+// }
+
+// Function Expression
+
+// let sayHi = function() {
+//     alert( "Привет" );
+// };
+
+// sayHi()
+
+// // стрелочная функция
+// let sayHi = (anotherFunc, text) => {
+//     anotherFunc()
+//     alert( text );
+// }
+
+// // const walking = () => {
+// //     console.log('я хожу')
+// // }
+
+// const running = () => {
+//     console.log('я бегу')
+// }
+
+// const wrapRunning = running
+
+// const newFunc = wrapRunning
+
+// sayHi(newFunc, 'Здарова!')
+
+let numbers = prompt('Введите значения');
+
+// суммирует любое количество значений
+const sum = (...numbers) => {
+    // ['5, 5']
+    let result = 0;
+
+    if (typeof numbers[0] === 'string') {
+        let arrOfStringNumbers;
+
+        if (numbers[0].includes(',')) {
+            arrOfStringNumbers = numbers[0].split(', ');
+        } else {
+            arrOfStringNumbers = numbers[0].split(' ');
+        }
+
+        console.log('arrOfStringNumbers', arrOfStringNumbers);
+
+        const arrOfNumbers = arrOfStringNumbers.map(item => Number(item));
+
+        console.log('arrOfNumbers', arrOfNumbers);
+
+        arrOfNumbers.forEach(item => (result = result + item));
+    } else {
+        numbers.forEach(item => (result = result + item));
+    }
+
+    return result;
+};
+
+console.log(sum(5, 5));
+
+if(arr.length === 0) {
+    
+}
+
+
+const SPEED_lIGHT = 30000
---
+++ object.js
@@ -0,0 +1,83 @@
+// let programmer = {
+//     name: 'Mihail',
+//     langs: ['c++', 'c#', 'php', 'Js', 'python'],
+//     level: 'middle',
+//     visa: false,
+//     myTehnique: [
+//         {
+//             name: 'PC',
+//         },
+//         {
+//             name: 'Fridge',
+//         },
+//     ],
+//     programming: example
+// };
+
+
+
+// const example =  function programming () {
+//     console.log(`${this.name} программирую! Не отвлекайте!`)
+// }
+
+// const key = prompt('введите ключ объекта')
+
+// console.log(programmer[key])
+
+
+
+
+// programmer.langs = [...programmer.langs, 'c' ]
+
+// programmer.programming()
+
+
+
+
+// ------------------------------
+
+// let a = 5;
+
+// let b = a;
+
+// b = 6
+
+// console.log(b)// 6
+// console.log(a) // 5
+
+//-----------------------------
+
+// let firstObj = {a: 5}
+
+// let secondObj = firstObj
+
+// secondObj.a = 6
+
+// console.log(secondObj)
+// console.log(firstObj)
+
+// ---------------------------
+
+// let firstObj = {a: 5}
+
+// let secondObj = firstObj
+
+// console.log(firstObj === secondObj)
+
+// ------------------------------
+
+// let firstObj = {
+//     a: 5,
+// }
+// // первый вариант копирования
+// let secondObj =  {...firstObj}
+
+// // второй вариант
+// // let secondObj =  Object.assign({}, firstObj)
+
+// secondObj.a = 6
+
+// console.log(firstObj)
+
+
+
